d18288eb92034386f8111266c0f65b233e9a17dd
Semon Horochih <HorSemAlex@yandex.ru>
2026-02-24 15:06:13 +0700

    Initial commit 2

---
+++ src/main.py
@@ -0,0 +1,87 @@
+
+
+# def pupum(a:int,b:int)->int:
+#     if a>b:
+#         return a
+#     return b
+
+# n = pupum(1,2)
+# print(n)
+
+# a = 30 + 2
+# b = 30 - 2
+# c = 30 * 2
+# d = 39 / 2
+# e = 39 % 2
+# f = 30 ** 2
+# g = 39 // 2
+
+# p = True or False
+# p = not p
+# print(p)
+
+# print(a,b,c,d,e,f,g)
+
+# massive = [98123823098,3,1,4,5]
+
+# print(massive[-3])
+
+# Перебирает массив
+# for element in massive:
+#     print(element)
+#     if element < 2:
+#         break
+#     print('Выполнился')
+# else:
+#     print('lol')
+
+
+# for i in range('a','z'):
+#     print(i)
+
+# Смотрит условие
+# while True:
+#     a = a + 1
+# else:
+#     print('aaaaaaa')
+
+## define - логическая операция
+# def xui(alibaba):
+#     и = alibaba+1000
+#     return и
+
+
+# ьшфзгкцщуг = xui(8999)
+
+# print('Сколько см у алибабы?? -',ьшфзгкцщуг)
+
+# a = int(input())
+
+# if a<1:
+#     print('1')
+# elif a < 6:
+#     print('2')
+# else:
+#     print('3')
+
+
+# d = {
+#     123: 'GIGI',
+#     'GIGI2': 1234,
+#     123: 'YOY'
+# }
+# print(d[123])
+# print(d['GIGI2'])
+# print(d)
+
+# m = [9,27,"dfgdfg",797,'687',5,6,True]
+# print(m)
+
+m = ['ewir908r384385408430']
+
+a = 5
+b = 10
+
+c = f'a={m},b={b}'
+
+print(c)
